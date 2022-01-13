6bdda017ef1f1a28bacd43b11c17988f7f067e15
Adhish Shrivastava <adhish70@gmail.com>
2022-01-13 10:31:23 -0500

    Initial Commit

---
+++ Problem-167 Robot Bounded In Circle.py
@@ -0,0 +1,11 @@
+# 1041. Robot Bounded In Circle
+# https://leetcode.com/problems/robot-bounded-in-circle/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+class Solution:
+    def isRobotBounded(self, instructions: str) -> bool:
+        pass
---
+++ Problem-168 Calculate Tax.py
@@ -0,0 +1,11 @@
+# Calculate Tax
+# https://www.glassdoor.co.in/Interview/Write-a-program-in-Python-to-calculate-tax-if-Salary-and-Tax-Brackets-are-given-as-list-in-the-form-10000-3-20000-QTN_2565898.htm
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+class Solution:
+    def calcTax(self, levels, salary):
+        pass
